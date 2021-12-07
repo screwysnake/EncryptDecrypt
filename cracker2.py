c5e5ada16b380467768a1d003efa9fe4b1a0768e
screwysnake <88514962+screwysnake@users.noreply.github.com>
2021-12-07 17:53:01 +0200

    gata

---
+++ cracker2.py
@@ -0,0 +1,60 @@
+import time
+def citire_fisier(nume_fisier):
+    with open(nume_fisier, "rb") as f:
+        data = f.read()
+        text = data.decode('utf-8')
+    return text
+
+
+def main():
+    text = citire_fisier("output_adversari")
+    # valid_chars este o lista de liste
+    valid_chars = []
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!?-,:;'[] " + '"' + "\n"
+    # alfabetul valid pentru input.txt este "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 " + "\n" + '"'
+    for cringe_char in text:
+        # vc este o lista care contine caracterele valide pentru parola
+        vc = []
+        # xoram fiecare caracter din alfabetul valid cu caracterul din output
+        # adaugam la lista vc doar rezultatele valide
+        for char in alphabet[:-13]:
+            if chr(ord(char) ^ ord(cringe_char)) in alphabet:
+                vc.append(char)
+        valid_chars.append(vc)
+
+    for lg in range(10, 16):
+        # fixam lungimea parolei (care poate fi 10-15 caractere)
+        password = []
+        found = 1
+        # cnt_ap este numarul de aparitii necesare al unui caracter din parola presupusa
+        cnt_ap = len(text) // lg
+        # cautam caracterul de pe pozitia x din parola
+        for x in range(lg):
+            freq = [0] * 74
+            # facem frecventa caracterului de pe pozitia x in fiecare lista de la pozitia x + k * lg, unde 0 <= k <= cnt_ap
+            for j in range(x, len(text), lg):
+                for char in valid_chars[j]:
+                    freq[alphabet.index(char)] += 1
+            ok = 0
+            # daca frecventa unui caracter coincide cu cnt_ap atunci acesta este caracterul corect al parolei pe pozitia x 
+            for i in range(len(freq)):
+                if freq[i] >= cnt_ap:
+                    password.append(alphabet[i])
+                    ok = 1
+                    break
+            if ok == 0:
+                found = 0
+                break
+        if found == 1:
+            return "".join(password)
+
+    return "hai ma frate ce-i asta"
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    password = main()
+    end_time = time.time()
+    print(password)
+    time_real = end_time - start_time
+    print(f"Timpul de executare este {time_real} secunde")
